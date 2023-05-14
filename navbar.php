6a2102b9e9f5af6dcb2a2de93d0b6c05f24d3f81
Yota Okabe <93124377+Yota-Okabe@users.noreply.github.com>
2023-05-15 00:56:58 +0900

    home.php を作成
    web siteのトップページとして、home.php を作成した。
    このページはメンバー以外が閲覧できる。
    メンバーはログインすることで、各機能が使えるようにする。

---
+++ navbar.php
@@ -0,0 +1,36 @@
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+        <img src="./parts/logo2.png" alt="" width="45" height="36" class="d-inline-block align-text-top">
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0 ">
+                <li class="nav-item">
+                    <a class="nav-link" aria-current="page" href="info.php">お知らせ</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="upload.php">画像アップロード</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="album.php">アルバム</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="bbs.php">掲示板</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        ログイン / ログアウト
+                    </a>
+                <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="#">login</a></li>
+                    <li><hr class="dropdown-divider"></li>
+                    <li><a class="dropdown-item" href="#">logout</a></li>
+                </ul>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
