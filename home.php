864f9f1f67982c492e6d61a336934cd5926313cc
Yota Okabe <93124377+Yota-Okabe@users.noreply.github.com>
2023-05-18 23:51:18 +0900

    homeを修正

--- home.php
+++ home.php
@@ -22,11 +22,13 @@
             <h1>About</h1>
             <div class="about-contents">
                 <div class="contents-left">
-                    <img src="./parts/content1.jpeg" alt="">
+                    <img src="./parts/content3.jpeg" alt="">
                 </div>
                 <div class="contents-right">
-                    <h2>Some text</h2>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In corrupti qui nesciunt tenetur perspiciatis velit quis esse ad culpa ipsum nam accusamus, eveniet optio reprehenderit iusto unde nobis deserunt magnam.<br>Lorem ipsum dolor sit amet consectetur adipisicing elit. In corrupti qui nesciunt tenetur perspiciatis velit quis esse ad culpa ipsum nam accusamus, eveniet optio reprehenderit iusto unde nobis deserunt magnam.</p>
+                    <h2>2016.04 設立</h2>
+                    <p>初めまして、F.C. Divariet 代表の赤間圭祐でございます。<br>
+                    当チームは2016年度より東京都リーグへの加盟を果たしました。はじめは漠然と「社会人になってもサッカーを続けたいなぁ。」という思いからスタートした活動だったのですが、今やドンドン人を巻き込み立派な組織に成長できたことを嬉しく思っています。<br>
+                    これからはさらなる発展を目指し、チームメンバー1人ひとりの意見を尊重しながら1つでも上の階級を目指し活動をしていく所存でございます。
                 </div>
             </div>
             <div class="about-contents">
@@ -34,8 +36,15 @@
                     <img src="./parts/content1.jpeg" alt="">
                 </div>
                 <div class="contents-right">
-                    <h2>Some text</h2>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In corrupti qui nesciunt tenetur perspiciatis velit quis esse ad culpa ipsum nam accusamus, eveniet optio reprehenderit iusto unde nobis deserunt magnam.<br>Lorem ipsum dolor sit amet consectetur adipisicing elit. In corrupti qui nesciunt tenetur perspiciatis velit quis esse ad culpa ipsum nam accusamus, eveniet optio reprehenderit iusto unde nobis deserunt magnam.</p>
+                    <h2>2022.11.13 『7年越しの思い "3部昇格"』</h2>
+                    <p>2022東京都社会人サッカー4部5ブロック、悲願の優勝！！<br>
+                    優勝には14点差以上での勝利が必要なDivariet。
+                    試合前から個人の仕事/試合開催に向けた調整ドラマの連続で、なんとか無事試合開催&参加にこじつけました。<br>
+                    数的優位を祈りながら雨の中グラウンドに向かうも、対戦相手はGK複数人も駆使し同数である事が判明。それでも、ここまで可能性を繋いだチームの勢いは止まりません。<br>
+                    開始4,5,7分と良いペースで見事な得点を重ねますが、次第に相手にボールを握られる場面も散見されはじめます。更に3点を追加しますが、直後にはまさかの失点…<br>
+                    ハーフタイムを迎えた時点で得点ペースとしては必要なアベレージより2点ビハインドの苦しい局面を迎えます。<br>
+                    それでも、今年のチームはよく我慢し、ポジティブな声も出るようになりました。後半に入ると更に攻勢を強め、小野/井口の2トップを中心にラスト10分までにリーチをかける8点を積み上げると、68分、井口がエリア内角度のないところから左足を振り抜くと逆サイドネットに決まり15点目！！更に田原悠がだめ押しの得点を無人のゴールに流し込み決定的な16点目。試合をそのまま締め、参入7年目で悲願の優勝を果たしました！<br>
+                    来シーズンからはカテゴリをあげて、新たな挑戦です！<br>
                 </div>
             </div>
         </section>
